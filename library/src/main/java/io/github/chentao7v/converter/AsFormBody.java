1ec8e02d6a0d43e2977090eaa175544f1e25578d
chentao <chentao2012>
2023-07-05 15:13:56 +0800

    优化注释

--- library/src/main/java/io/github/chentao7v/converter/AsFormBody.java
+++ library/src/main/java/io/github/chentao7v/converter/AsFormBody.java
@@ -9,12 +9,11 @@ import okhttp3.FormBody;
 import retrofit2.http.Body;
 
 /**
- * 标识是否将{@link Body}转换为{@link FormBody}而不是类型为 application/json 的 body。
+ * 标识是否将 {@link Body} 转换为 {@link FormBody} 而不是类型为 application/json 的 body。
  * <p>
- * 被该注解标识的类中，会将有值的成员变量的名称(如果该字段有{@link SerializedName}的标识则优先取注解上的值)作为 key，
- * 该成员变量的值为value，添加到{@link FormBody}中。
+ * 被该注解标识的类中，会将有值的成员变量的名称(如果该字段有 {@link SerializedName} 的标识则优先取注解上的值)作为 key，
+ * 该成员变量的值为 value，添加到 {@link FormBody} 中。
  * </p>
- * <br>
  * create by chentao on 2022-08-24.
  */
 @Target(ElementType.TYPE)
